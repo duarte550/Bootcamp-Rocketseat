39b1508fc02f8083d9031b139f7870d7840f7902
Gabriel Duarte <duartegabriel550@gmail.com>
2020-06-16 23:56:43 -0300

    Desafio Terminado

--- gostack-template-conceitos-nodejs/src/app.js
+++ gostack-template-conceitos-nodejs/src/app.js
@@ -19,7 +19,7 @@ app.get("/repositories", (request, response) => {
 
 app.post("/repositories", (request, response) => {
     const {title, url, techs} = request.body
-    const repository = {id:uuid(),title,url,techs}
+    const repository = {id:uuid(),title,url,techs,"likes":0}
     repositories.push(repository)
 
     return response.json(repository)
@@ -28,25 +28,54 @@ app.post("/repositories", (request, response) => {
 
 app.put("/repositories/:id", (request, response) => {
     const {id} = request.params
-    console.log(repositories)
+    const {title, url,techs} = request.body
+
+    
     repositoryIndex = repositories.findIndex(repository => repository.id == id)
-    console.log(repositoryIndex)    
+     
     if (repositoryIndex <0){
       return response.status(400).json({error: 'project not found'})
     }
-    repositories[repositoryIndex].title = "ALTEREI"
     repository = repositories[repositoryIndex]
+    const likes = repository.likes
+    
+    repository = {id, title, url, techs, likes}
+
+    repositories[repositoryIndex] = repository
+    
     
 
     return response.json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  
+  const {id} = request.params
+    
+  repositoryIndex = repositories.findIndex(repository => repository.id == id)
+     
+  if (repositoryIndex <0){
+      return response.status(400).json({error: 'project not found'})
+  }
+  repository = repositories[repositoryIndex]
+  repositories.splice(repositoryIndex,1)
+
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params
+    
+    repositoryIndex = repositories.findIndex(repository => repository.id == id)
+     
+    if (repositoryIndex <0){
+      return response.status(400).json({error: 'project not found'})
+    }
+    repositories[repositoryIndex].likes += 1 
+    repository = repositories[repositoryIndex]
+    
+
+    return response.json(repository)
 });
 
 module.exports = app;
